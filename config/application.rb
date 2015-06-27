e3b6c09383d583d377ce5816d5eb648b41e8c8ed
ismail akbudak <akbudak.ismail@yandex.com>
2015-06-27 21:44:33 +0300

    remove change language buttons

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Halisaa
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :tr
     config.exceptions_app = self.routes
 
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
