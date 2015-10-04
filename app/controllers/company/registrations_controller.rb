628dfc0f2751e7abefe99853858784c340a0005f
ismail akbudak <akbudak.ismail@yandex.com>
2015-10-04 18:15:19 +0300

    ui improvement

--- app/controllers/company/registrations_controller.rb
+++ app/controllers/company/registrations_controller.rb
@@ -3,19 +3,23 @@ class Company::RegistrationsController < Devise::RegistrationsController
   before_action :authenticate_company!
   before_action :redirect_company, only: [:destroy]
   add_breadcrumb I18n.t('dock.dashboard'), :company_root_path
-
-  def edit
-    render layout: 'company/application'
-  end
+  layout :layout_by_resource
 
   private
+    def layout_by_resource
+      if company_signed_in?
+        'company/application'
+      else
+        'application'
+      end
+    end
 
-  def redirect_company
-      redirect_to company_root_path
-  end
+    def redirect_company
+        redirect_to company_root_path
+    end
 
-  def after_update_path_for(resource)
-    company_root_path
-  end
+    def after_update_path_for(resource)
+      company_root_path
+    end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,11 @@
 module ApplicationHelper
-  def success( data, message: "Success!", error: false  )
+  def success( data, message: 'Success!', error: false  )
     { :status => 200, :message => message, :error => error, :data => data }
   end
   def money( amount  )
      t('money.try', amount: amount)
+  end
+  def set_active(array, class_name: 'active')
+    array.to_s.include?(request.path) ? class_name : ''
   end
 end
